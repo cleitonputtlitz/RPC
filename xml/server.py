b24befe88dfd69df4415d5be40a3ef7ce46a9ba3
Cleiton Puttlitz <cleitonputtlitz@gmail.com>
2023-04-14 15:10:47 -0300

    Update server.py

--- xml/server.py
+++ xml/server.py
@@ -2,7 +2,8 @@ from xmlrpc.server import SimpleXMLRPCServer
 
 def exec(n):
     print("Requisição recebida com tamanho: " + str(len(n)))
-    return n
+    x = n
+    return x
 
 server = SimpleXMLRPCServer(("0.0.0.0", 8000))  #0.0.0.0 ao inves de localhost para funcionar entre diferentes maquinas
 print("Listening on port 8000...")
