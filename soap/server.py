3553932edca46bfc41b2570239362111b7c5c352
Cleiton Puttlitz <cleitonputtlitz@gmail.com>
2023-04-14 18:18:25 -0300

    Update server.py

--- soap/server.py
+++ soap/server.py
@@ -4,42 +4,48 @@ from spyne import Application, rpc, ServiceBase, Iterable, Integer, Unicode, Str
 
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
+import psutil
+import time
 
-class HelloWorldService(ServiceBase):
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def say_hello(ctx, name, times):
-        """Docstrings for service methods appear as documentation in the wsdl.
-        <b>What fun!</b>
-        @param name the name to say hello to
-        @param times the number of times to say hello
-        @return the completed array
-        """
+#143.54.49.122
+global last_time
+last_time = 0
 
-        for i in range(times):
-            yield u'Hello, %s' % name
+def CPU_Memory_Usage():
+    global last_time
+    if(time.time() - last_time >= 1):
+        last_time = time.time()
+        #Obtém informações de uso da CPU
+        cpu_percent = psutil.cpu_percent()
+        # Obtém informações de uso da memória
+        memory = psutil.virtual_memory()
+        memory_percent = memory.percent
+        f.write(str("{:.6f}".format(cpu_percent)) + "; " + str("{:.6f}".format(memory_percent))+";\n")
+
 
 class returnReceive(ServiceBase):
     #List(min_occurs=1, max_occurs='unbounded', nillable=False)
     @rpc(Iterable(Unicode), _returns=Iterable(Unicode))
     def return_hello(ctx, parm):
-        return parm
+        CPU_Memory_Usage()
+        x = parm
+        return x
 
-application = Application([HelloWorldService, returnReceive], 'spyne.examples.hello.soap',
+application = Application([returnReceive], 'spyne.examples.hello.soap',
                 in_protocol=Soap11(),
                 out_protocol=Soap11())
 
 wsgi_application = WsgiApplication(application)
 
 if __name__ == '__main__':
-    import logging
-
+    
     from wsgiref.simple_server import make_server
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
+    print("listening  on port 8000")
+    f = open("log_server.csv", "a")
+    f.write("cpu; memoria;\n")
 
-    logging.info("listening to http://127.0.0.1:8000")
-    logging.info("wsdl is at: http://localhost:8000/?wsdl")
-
-    server = make_server('127.0.0.1', 8000, wsgi_application)
+    server = make_server('0.0.0.0', 8000, wsgi_application)
     server.serve_forever()
+    
+    f.close()
